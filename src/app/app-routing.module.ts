3e1e4661055d94149ce7d019fd74eb4c6f82502a
sai korthivada <sai.korthivada@xcubelabs.com>
2020-09-05 12:33:33 +0530

    Implemented loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PaymentComponent } from 'src/app/views/admin-views/payment/payment.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'orders',
+    redirectTo: 'payments',
     pathMatch: 'full'
   },
   {
     path: 'orders',
     loadChildren: () => import('./views/admin-views/orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: 'payments',
+    component: PaymentComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PaymentComponent } from './views/admin-views/payment/payment.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PaymentComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
